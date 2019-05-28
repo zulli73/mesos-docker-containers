392678a6bd844be956e7c9a62490f6cef07221dc
Raphael Zulliger <raphael.zulliger@besi.com>
2019-05-28 06:12:59 +0200

    Got rid of everything I presumably don't require for my "Data Science" use case
    This is a "work in progress" commit as Zeppelin/PySpark is currently not working

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 tag=$1
-prefix=datastrophic
+prefix=zulli73
 
-marathon_tag=1.3.6
-chronos_tag=3.0.1
-kafka_tag=0.10.0.0
-spark_tag=2.1.0
-zeppelin_tag=0.7.0
+spark_tag=2.4.3
+zeppelin_tag=0.8.1
 
 dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 home="$(dirname "$dir")"
@@ -25,17 +22,12 @@ docker build --tag ${prefix}/mesos:${tag}                                  ${hom
 docker build --tag ${prefix}/mesos-java:${tag}                             ${home}/mesos-java
 docker build --tag ${prefix}/mesos-master:${tag}                           ${home}/mesos-master
 docker build --tag ${prefix}/mesos-slave:${tag}                            ${home}/mesos-slave
-docker build --tag ${prefix}/marathon:${marathon_tag}                      ${home}/marathon
 
 if [ "$tag" = "latest" ]
 then
-   docker build --tag ${prefix}/chronos:${tag}                ${home}/chronos
-   docker build --tag ${prefix}/mesos-kafka:${tag}            ${home}/mesos-kafka
    docker build --tag ${prefix}/mesos-spark:${tag}            ${home}/mesos-spark
    docker build --tag ${prefix}/mesos-spark-zeppelin:${tag}   ${home}/mesos-spark
 else
-   docker build --tag ${prefix}/chronos:mesos-${tag}-chronos-${chronos_tag}                            ${home}/chronos
-   docker build --tag ${prefix}/mesos-kafka:mesos-${tag}-kafka-${kafka_tag}                            ${home}/mesos-kafka
    docker build --tag ${prefix}/mesos-spark:mesos-${tag}-spark-${spark_tag}                            ${home}/mesos-spark
-   docker build --tag ${prefix}/mesos-spark:mesos-${tag}-spark-${spark_tag}-zeppelin-${zeppelin_tag}   ${home}/mesos-spark-zeppelin
+   docker build --tag ${prefix}/mesos-spark-zeppelin:mesos-${tag}-spark-${spark_tag}-zeppelin-${zeppelin_tag}   ${home}/mesos-spark-zeppelin
 fi
